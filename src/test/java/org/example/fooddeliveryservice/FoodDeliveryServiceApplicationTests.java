1120338c631381c044ec7f271d7acda9e8ba0406
John <pandastars535@gmail.com>
2024-07-19 05:43:08 +0300

    Created shell of the project

---
+++ src/test/java/org/example/fooddeliveryservice/FoodDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fooddeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
